9f533c0d10691fa62190faaced6afbd01956b2b7
himanshu1138 <85988441+himanshu1138@users.noreply.github.com>
2021-06-17 21:47:23 +0530

    Add files via upload

---
+++ min_scalar_mul_for_product_of_n_matrices.py
@@ -0,0 +1,15 @@
+import sys
+ 
+def Calc(p,i,j):
+  if i==j:
+    return 0
+  m=sys.maxsize
+  for k in range(i,j):
+    count=(Calc(p,i,k) + Calc(p,k+1,j) + p[i-1] * p[k] * p[j])
+    if count<m:
+      m=count
+  return m
+
+print("Enter dimensions of matrices:",end=" ")
+l=[int(i) for i in input().split()]
+print("Minimum number of scalar multiplication:",Calc(l,1,len(l)-1))
